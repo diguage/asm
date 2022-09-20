23f0e1dfe89b076ff1260e5ac9a7b4ed7b1c9a4e
diguage <leejun119@gmail.com>
2022-09-20 09:41:33 +0800

    增加新内容

--- turman/src/main/java/com/diguage/truman/Bean.java
+++ turman/src/main/java/com/diguage/truman/Bean.java
@@ -18,4 +18,12 @@ public class Bean {
             throw new IllegalArgumentException();
         }
     }
+
+    public static void sleep(long d) {
+        try {
+            Thread.sleep(d);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
